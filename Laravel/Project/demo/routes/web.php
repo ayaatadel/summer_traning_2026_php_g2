f4547a25ff56d414f73c8fa03c1f6b3bddf69e72
ayaatadel <ayaatadel128@gmail.com>
2026-08-23 12:02:30 +0300

    day1 laravel

--- Laravel/Project/demo/routes/web.php
+++ Laravel/Project/demo/routes/web.php
@@ -5,3 +5,88 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+route::get('/students',function()
+{
+  $students = [
+    [
+        "id" => 1,
+        "name" => "leena",
+        "email" => "leena@gmail.com",
+    ],
+    [
+        "id" => 2,
+        "name" => "salma",
+        "email" => "salma@gmail.com",
+    ],
+    [
+        "id" => 3,
+        "name" => "login",
+        "email" => "login@gmail.com",
+    ],
+    [
+        "id" => 4,
+        "name" => "mohammed",
+        "email" => "mohammed@gmail.com",
+    ],
+
+];
+// return view('allStudents',["students"=> $students]);
+return view('allStudents',compact("students"));
+//     foreach ($students as $student) {
+//     // var_dump($student);
+//     // echo "<br> ******************************* <br>";
+
+//     # code...
+// }
+
+});
+
+
+//********************* Get Single student  (task) */
+route::get('/students/{id}',function()
+{
+      $students = [
+    [
+        "id" => 1,
+        "name" => "leena",
+        "email" => "leena@gmail.com",
+    ],
+    [
+        "id" => 2,
+        "name" => "salma",
+        "email" => "salma@gmail.com",
+    ],
+    [
+        "id" => 3,
+        "name" => "login",
+        "email" => "login@gmail.com",
+    ],
+    [
+        "id" => 4,
+        "name" => "mohammed",
+        "email" => "mohammed@gmail.com",
+    ],
+
+];
+    // $student;//
+    return view('student',compact('student'));
+});
+
+// route::get('url',action); // action ===> function  || controller
+
+/***
+ *
+ * get
+ * post
+ * put
+ * update
+ * edit
+ * delete
+ */
